df2a6d96dc38502854dd0e3638a2018afbfc776e
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-05-07 16:01:55 +0500

    yakunlandi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CarsModule } from './modules';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CarsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: path.join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    CarsModule,
+  ],
 })
-
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NotAcceptableException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.APP_PORT || 4000;
+  app.enableCors({
+    allowedHeaders: ['authorization'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    optionsSuccessStatus: 200,
+    origin: (reqOrigin, cb) => {
+      const allowedOrigins = process.env.CROS_ORIGINS
+        ? process.env.CORS_ORIGINS?.split(',')
+        : ['*'];
+
+      if (allowedOrigins?.includes(reqOrigin) || allowedOrigins?.includes('*'))
+        return cb(null, reqOrigin);
+      else
+        cb(
+          new NotAcceptableException(
+            `${reqOrigin}'❌`,
+          ),
+        );
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
